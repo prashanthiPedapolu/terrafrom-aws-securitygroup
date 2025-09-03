61872690c0efe772351aa1a7622f96931ef12d1c
prashanthiPedapolu <pedapolu.prashanthi24@gmail.com>
2025-09-03 15:39:58 -0400

    terraform

---
+++ sg.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "main" {
+  name        = "${var.project}-${var.environment}-${var.sg_name}"
+  description = var.sg_description
+  vpc_id      = var.vpc_id
+  tags = merge(
+    var.sg_tags,
+    common_tags,
+    {
+        Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
+}
